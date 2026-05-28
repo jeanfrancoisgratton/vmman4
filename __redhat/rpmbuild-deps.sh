f08bc104ff4b3fa59ce2cde0d770204fb4297583
Binary package builder <builder@famillegratton.net>
2026-05-28 07:58:45 -0400

    fixed paths in script

--- __redhat/rpmbuild-deps.sh
+++ __redhat/rpmbuild-deps.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 echo "Installing BuildRequires dependencies";echo
-grep ^BuildRequires nxtools.spec |awk -F\: '{print "sudo dnf install -y"$2}'|sed -e 's/,/ /g' | sh
+grep ^BuildRequires vmman4.spec |awk -F\: '{print "sudo dnf install -y"$2}'|sed -e 's/,/ /g' | sh
 echo;echo;echo "Done. Now installing the Go binaries"
 #/opt/bin/install_golang.sh $(grep ^go src/go.mod | awk '{print $2}') amd64
 
 
-export VER=`cat go.version`
+export VER=`cat ../go.version`
 export ARCH=${1:-"amd64"}
 
 echo "Fetching archive..."
