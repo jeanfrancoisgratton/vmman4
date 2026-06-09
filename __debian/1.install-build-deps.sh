8b9ee4bfa9dde261ee627731e1f468eb2cc44acc
Binary package builder <builder@famillegratton.net>
2026-06-09 11:10:59 -0400

    updated build-deps, second attempt

--- __debian/1.install-build-deps.sh
+++ __debian/1.install-build-deps.sh
@@ -5,10 +5,9 @@
 # This docker container has been stripped down as much as possible, and works for GO software
 # The following extra packages might be needed for languages other than GO :
 
-sudo apt install -y gcc fakeroot devscripts build-essential
-
 echo "Installing dependencies";echo
 sudo apt-get update && sudo apt update -y
+sudo apt install -y gcc fakeroot devscripts build-essential
 echo;echo;echo "Done. Now installing the Go binaries"
 sudo rm -rf /opt/go-versions ; sudo mkdir -p /opt/go-versions
 /opt/bin/goget.sh `cat ../go.version`
